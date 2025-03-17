7605a70895e0f4e17456d1e472d4237fc5f60209
Gwuan <jelle388@gmail.com>
2025-03-17 10:07:48 +0100

    Included & configurated SteamAPI

--- Minigin/Renderer.cpp
+++ Minigin/Renderer.cpp
@@ -2,6 +2,7 @@
 #include "Renderer.h"
 
 #include <iostream>
+#include <steam_api_common.h>
 
 #include "SceneManager.h"
 #include "Texture2D.h"
@@ -48,6 +49,8 @@ void dae::Renderer::Render() const
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui_ImplSDL2_NewFrame();
 	ImGui::NewFrame();
+
+	SteamAPI_RunCallbacks(); 
 
 	SceneManager::GetInstance().Render();
 
